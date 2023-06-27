fdf6cca68d09df4f76d30ae1a3ecb132ddef7a04
Petar Kresoja <petarkresoja5@gmail.com>
2023-06-27 13:39:13 +0200

    Updated scripts

--- run-rest.sh
+++ run-rest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/petar/Desktop/rasa-test
+cd /home/petar/rasa-singi-upis
 source /home/petar/anaconda3/etc/profile.d/conda.sh
 conda activate rasabot
 rasa run --enable-api --cors "*"
