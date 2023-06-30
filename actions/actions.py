be3242f261749ccc3bcae3f7340b33717f99981c
Petar Kresoja <petarkresoja5@gmail.com>
2023-06-30 17:53:52 +0200

    Add fallback actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,26 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionDefaultFallback(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self):
+        return "action_default_fallback"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(
+            "Na žalost, nisam u mogućnosti da Vam odgovorim na ovo pitanje. Molim Vas da kontakrite studentski servis na adresi: studije@singidunum.ac.rs")
+        return []
+
+
+class ActionUnlikelyIntent(Action):
+
+    def name(self):
+        return "action_unlikely_intent"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(
+            "Na žalost, nisam u mogućnosti da Vam odgovorim na ovo pitanje. Molim Vas da kontakrite studentski servis na adresi: studije@singidunum.ac.rs")
+        return []
