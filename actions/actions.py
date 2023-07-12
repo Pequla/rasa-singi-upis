893a661485fff33ed20b3159751eb0b71d8cc0d4
Petar Kresoja <petarkresoja5@gmail.com>
2023-07-12 15:57:37 +0200

    Changed default action

--- actions/actions.py
+++ actions/actions.py
@@ -1,15 +1,16 @@
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action
-from rasa.shared.core.events import ConversationPaused, UserUtteranceReverted
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
 
 
 class ActionDefaultFallback(Action):
 
-    def name(self):
+    def name(self) -> Text:
         return "action_default_fallback"
 
-    def run(self, dispatcher, tracker, domain):
+    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text,Any]):
         dispatcher.utter_message(
             "Nažalost, nisam u mogućnosti da Vam odgovorim na ovo pitanje. Molim Vas da kontaktirate studentski servis na adresi: studije@singidunum.ac.rs")
-        return [ConversationPaused(), UserUtteranceReverted()]
+        return [UserUtteranceReverted()]
