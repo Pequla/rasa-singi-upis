92b5e0fe86a9a2661fec5e3d4c5c81c4c43546e1
Petar Kresoja <petarkresoja5@gmail.com>
2023-07-14 14:29:39 +0200

    Add Croatian language model

--- run-actions.sh
+++ run-actions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/petar/rasa-singi-upis
-source /home/petar/anaconda3/etc/profile.d/conda.sh
-conda activate rasabot
+#source /home/petar/anaconda3/etc/profile.d/conda.sh
+#conda activate rasabot
+source /home/petar/rasanew/bin/activate
 rasa run actions
